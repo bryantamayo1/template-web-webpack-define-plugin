c9888340aea931c67c0a5cacdf761898c1b0bf9d
Bryan <david_bryan34@hotmail.com>
2021-11-21 23:34:04 +0000

    webpack.DefinePlugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const { webpack } = require('webpack');
+const webpack  = require('webpack');
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
