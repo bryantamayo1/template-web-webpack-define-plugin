b285cac0a26d4b3458c66eeebdfe7a183d2d6ed5
Bryan <david_bryan34@hotmail.com>
2021-11-29 22:13:16 +0000

    Uncommented library MiniCssExtractPluginin webpack.common.js

--- config-webpack/webpack.common.js
+++ config-webpack/webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // const devMode = process.env.NODE_ENV !== "production";
 
 /** @type {import('webpack').Configuration} */
