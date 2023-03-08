c3e6b3dda0e03d99480a559448b9e6c370c3db5f
Bryan <david_bryan34@hotmail.com>
2023-03-08 23:53:23 +0000

    Change files of webpack Ok

--- config-webpack/webpack.prod.js
+++ config-webpack/webpack.prod.js
@@ -1,20 +1,59 @@
-const { merge } = require("webpack-merge")
-const common = require('./webpack.common');
-const webpack  = require('webpack');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require("terser-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin    = require('css-minimizer-webpack-plugin');
+const MiniCssExtractPlugin  = require('mini-css-extract-plugin');
+const TerserPlugin          = require("terser-webpack-plugin");
+const CopyPlugin            = require("copy-webpack-plugin");
+const path                  = require('path');
+const HtmlWebpackPlugin     = require('html-webpack-plugin');
+const webpack               = require('webpack');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const devMode            = process.env.NODE_ENV !== "production";
 
 /** @type {import('webpack').Configuration} */
-const prod = {
+module.exports = {
+    entry: "./src/js/index.js",
+    output: {
+        path: path.resolve(__dirname, '../build'),
+        filename: "index.[contenthash].js",
+        clean: true,
+        // Keep original file’s name
+        assetModuleFilename: "[name][ext]",
+    },
     mode: 'production',
     module: {
         rules: [
-          {
-            test: /\.css$/i,
-            use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-        },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.m?js$/,
+                include: path.resolve(__dirname, '../src/js'),
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                    }
+            },
+            // {
+            //     test: /\.css$/i,
+            //     use: [devMode? 'style-loader': MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+            // },
+            {
+                type: "asset/resource",
+                test: /\.(jpg|jpeg|png|gif|svg|ico)$/i
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+            },
         ]
     },
     // devtool: 'source-map' // It's recommended
@@ -51,8 +90,9 @@ const prod = {
         // }
     },
     plugins:[
-        new webpack.DefinePlugin({
-            'URL': JSON.stringify('http://my-web-prod')
+        new HtmlWebpackPlugin({
+            // filename: 'index.html',
+            template: './src/index.html'
         }),
         new MiniCssExtractPlugin({
             filename: '[name].[contenthash].css'
@@ -63,7 +103,8 @@ const prod = {
                 { from: './files-to-build', to: './' }
             ]
         }),
+        new webpack.DefinePlugin({
+            'URL': JSON.stringify('http://my-web-prod')
+        })
     ]
 }
-
-module.exports = merge(common, prod);
